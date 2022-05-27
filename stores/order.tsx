69c2678b29c469c8a335a07f5b2f3d6f592561ff
Adam Sir <donuteeze@gmail.com>
2022-05-27 15:29:14 +0200

    feat(store): add persistent storage middleware

--- stores/order.tsx
+++ stores/order.tsx
@@ -2,6 +2,8 @@ import create from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 import type { Product } from '../pages/api/products'
 
+const storeMiddleware = f => devtools(persist(f))
+
 type OrderStore = {
   items: Product[];
   setItems: (items: Product[]) => void;
@@ -20,25 +22,27 @@ const addItem = (items: Product[], item: Product, quantity: number): Product[] =
 
 const clearItems = () => []
 
-const useStore = create<OrderStore>(
-  (set): OrderStore => ({
-    items: [],
-    setItems: (items: Product[]) =>
-      set((state) => ({
-        ...state,
-        items,
-      })),
-    addItem: (item: Product, quantity: number) =>
-      set((state) => ({
-        ...state,
-        items: addItem(state.items, item, quantity)
-      })),
-    clearItems: () =>
-      set((state) => ({
-        ...state,
-        items: clearItems()
-      }))
-  })
-);
+const useStore = create<OrderStore>()(persist((set, get): OrderStore => ({
+  items: [],
+  setItems: (items: Product[]) =>
+    set((state) => ({
+      ...state,
+      items,
+    })),
+  addItem: (item: Product, quantity: number) =>
+    set((state) => ({
+      ...state,
+      items: addItem(state.items, item, quantity)
+    })),
+  clearItems: () =>
+    set((state) => ({
+      ...state,
+      items: clearItems()
+    }))
+}), {
+  name: 'bla',
+  getStorage: () => localStorage
+}))
+
 
 export default useStore
